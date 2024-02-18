b4e34d7ea242736d52bb9763bb10bb1f29d356ef
gd-salle <gerardojr.salle@gmail.com>
2024-02-18 21:57:02 +0800

    fixed some typos

--- js/naturalgameplay.js
+++ js/naturalgameplay.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const questions = [
         // NATURAL CATEGORY
         { category: 'Natural', question: 'Pag-agi ni kuwaw\nHinigop an sabaw', answer: 'Balangaw', status: true},
-        { category: 'Natural', question: 'Nag-abot si boy negro\nNagkagaradan si mga tao', answer: 'Banggi', status: true},
+        { category: 'Natural', question: 'Nag-abot si Boy Negro\nNagkagaradan si mga tao', answer: 'Banggi', status: true},
         { category: 'Natural', question: 'Aram mo pero dai mo masasabotan\nDai mo nasasabotan pero aram mo', answer: 'Kagadanan', status: true},
         { category: 'Natural', question: 'Bakong tao, bakong hayop\nPero nagsisigarilyo', answer: 'Bulkan', status: true},
     ]
